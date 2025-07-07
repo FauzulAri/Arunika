c401aeeec39d5a71fedebdd5101c6d27e111eeec
FauzulAri <fauzularianto@students.amikom.ac.id>
2025-07-07 13:56:10 +0700

    update login user process

--- controller/user_login_process.php
+++ controller/user_login_process.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+session_start();
 include_once $_SERVER['DOCUMENT_ROOT'] . '/Arunika/config/connect.php';
 
 // Mengecek apakah form login telah disubmit
@@ -21,13 +25,19 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
 
         // Memverifikasi password yang dimasukkan dengan password yang di-hash
         if (password_verify($password, $hashed_password)) {
-            session_start();
             $_SESSION['user_id'] = $user_id;
             $_SESSION['nama'] = $nama;
             $_SESSION['email'] = $email;
             $_SESSION['role'] = 'user';
-            header('Location: /Arunika/view/user/home/index.php');
-            exit();
+            // Debug session sebelum redirect
+            echo '<pre>SESSION: '; print_r($_SESSION); echo '</pre>';
+            // Cek apakah session benar-benar terset
+            if (!headers_sent()) {
+                header('Location: /Arunika/view/user/home/index.php');
+                exit();
+            } else {
+                echo '<br>Header sudah terkirim, tidak bisa redirect.';
+            }
         } else {
             echo "<script>alert('Email atau kata sandi salah!'); window.location='/Arunika/view/auth/login.php';</script>";
         }

98e41f7eecec847f094cc99f6cadd3724c662a9b
FauzulAri <fauzularianto@students.amikom.ac.id>
2025-07-07 14:44:37 +0700

    Update master.php

--- view/user/master.php
+++ view/user/master.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 if (!isset($title)) {
     $title = 'Default Title';
 }
@@ -6,7 +9,7 @@ include $_SERVER['DOCUMENT_ROOT'] . '/Arunika/view/user/layout/header.php';
 ?>
 <!-- CONTENT -->
 <div id="content">
-    <?php if (isset($content)) { echo $content; } ?>
+    <?php if (isset($content)) { echo $content; } else { echo '<div style="color:red">Konten tidak ditemukan (variabel $content kosong)</div>'; } ?>
 </div>
 <!-- END CONTENT -->
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/Arunika/view/user/layout/footer.php'; ?>

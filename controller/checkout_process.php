c317a4e29df9ce2d4f20d10af5086575e67e2a53
FauzulAri <fauzularianto@students.amikom.ac.id>
2025-07-07 23:38:22 +0700

    Update checkout_process.php

--- controller/checkout_process.php
+++ controller/checkout_process.php
@@ -61,7 +61,7 @@ $nomor_order = 'ORD-' . date('YmdHis') . '-' . rand(1000,9999);
 
 // Insert ke database dengan nomor_order tersebut
 $stmt = $conn->prepare("INSERT INTO orders (user_id, nomor_order, total_harga, status_order, metode_pembayaran, alamat_pengiriman, nama_penerima, no_hp_penerima, catatan) VALUES (?, ?, ?, 'pending', ?, ?, ?, ?, ?)");
-$stmt->bind_param('isdssssss', $user_id, $nomor_order, $total, $metode_pembayaran, $alamat_pengiriman, $nama_penerima, $no_hp_penerima, $catatan);
+$stmt->bind_param('isdsssss', $user_id, $nomor_order, $total, $metode_pembayaran, $alamat_pengiriman, $nama_penerima, $no_hp_penerima, $catatan);
 $stmt->execute();
 $new_order_id = $stmt->insert_id;
 $stmt->close();
